66e40c4525b4b4595a2a15997ecd0f444599b82a
Isac Moura <contatoisacmoura@outlook.com>
2018-10-29 12:41:09 -0300

    Fixing errors of traduction and fixing the bug of moving folders

--- python-mover.py
+++ python-mover.py
@@ -5,7 +5,7 @@ lang = 'en-US'
 lang_strings = {
     "pt-BR" : {
         "path" : "Digite o caminho do repositório\n",
-        "image_cnt" : "Quantas imagens deseja mover?\n",
+        "image_cnt" : "Quantos arquivos deseja mover?\n",
         "create_dir" : "\nDeseja criar um novo diretório ou mover as imagens para um diretório existente?\n1 para CRIAR\n2 para MOVER",
         "new_dir_name" : 'Digite o nome do novo diretório\n',
         "new_dir_path" :  'Digite o local do diretório\n',
@@ -15,11 +15,11 @@ lang_strings = {
         "finalized": '\nSistema finalizado!'
     },
     "en-US" : {
-        "path" : "Type the name of the repository\n",
-        "image_cnt" : "How many images do you want to move?\n",
+        "path" : "Type the path of the repository\n",
+        "image_cnt" : "How many files do you want to move?\n",
         "create_dir" : "\nDo you want to create a new directory or move the images to an existing directory?\n1 for CREATE\n2 for MOVE\n",
         "new_dir_name" : 'Type the name of the new directory\n',
-        "new_dir_path" :  'Type the path of the new directory\n',
+        "new_dir_path" :  'Type the path of the directory\n',
         "moving-image" : 'Moving image - ',
         "moving-image-to" : ' to ',
         "continue" : '\nDo you wish to continue?\n1 for YES\n2 for NO\n',
@@ -47,10 +47,11 @@ while True:
         
     #Percorrer imagens e movê-las (começa em 0 até total a ser movido)
     for file_names in files[:total]:
-        print(strings["moving-image"] + file_names + strings["moving-image-to"] + mvdir)
-        shutil.move(file_names, mvdir)
+        #Excluindo os diretórios da lista de arquivos a serem movidos
+        if not(os.path.isdir(file_names)):
+            print(strings["moving-image"] + file_names + strings["moving-image-to"] + mvdir)
+            shutil.move(file_names, mvdir)
     
-    #Pergunta se o usuário deseja mover mais alguma leva de arquivos
     choice = int(input(strings["continue"]))
     if choice == 2:
         break
